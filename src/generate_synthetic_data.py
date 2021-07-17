16a3acbed5b7e18649d2782eb7fa5d42526e55ea
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2021-07-17 14:00:57 +0300

    Change var name

--- src/generate_synthetic_data.py
+++ src/generate_synthetic_data.py
@@ -1,10 +1,10 @@
 import numpy as np
 from scipy.stats import bernoulli
 
-def boolean_concept_uniform_distribution(boolean_function, p, dataset_size, no_variables):
+def boolean_concept_uniform_distribution(conjunctions, p, dataset_size, no_variables):
     X = bernoulli.rvs(p, size =(dataset_size,no_variables))
     Y = np.zeros(dataset_size)
-    for conjunction in boolean_function:
+    for conjunction in conjunctions:
             conjunction_values = np.prod(X[:,conjunction], axis=1)
             Y  = Y + conjunction_values
     Y =  np.clip(Y, 0.0, 1.0)
