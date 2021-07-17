f0069f19a0da6a1b617846283fa2b08ce2a8a7cf
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2021-07-17 14:00:11 +0300

    Add experiments with synthetic data

--- experiments/experiments_synthetic_data.py
+++ experiments/experiments_synthetic_data.py
@@ -3,23 +3,55 @@ sys.path.append('../')
 
 from DeepInterpretablePolynomialNeuralNetwork.src.generate_synthetic_data import boolean_concept_uniform_distribution
 from DeepInterpretablePolynomialNeuralNetwork.src.deep_interpretable_polynomial_neural_network import DeepInterpretablePolynomialNeuralNetwork, GrowthPolicy
+from DeepInterpretablePolynomialNeuralNetwork.src.evaluation_tools import EvaluationTools
 
-def basic_experiment_no_growth():
+def experiment_no_growth_degree2_no_fixed_margin():
+    # Data 
+    conjunctions = [[1,2],[1,3]]
+    p = 0.5
+    dataset_size = 1000
+    no_variables = 20
+    X, Y = boolean_concept_uniform_distribution(conjunctions, p, dataset_size, no_variables)
+
+    # Model
     d_max = 2
     balance = 2.0
     lambda_param = 10.0
     ro = 1.0
     fixed_margin = False
-    sip = DeepInterpretablePolynomialNeuralNetwork(d_max=d_max, lambda_param=lambda_param, balance=balance, fixed_margin=fixed_margin, ro=ro, derivative_magnitude_th=0.0, coeff_magnitude_th=0.0, 
+    dipnn = DeepInterpretablePolynomialNeuralNetwork(d_max=d_max, lambda_param=lambda_param, balance=balance, fixed_margin=fixed_margin, ro=ro, derivative_magnitude_th=0.0, coeff_magnitude_th=0.0, 
                                         max_no_terms_per_iteration=10, max_no_terms=200, growth_policy=GrowthPolicy.ALL_TERMS)
-    X, Y = boolean_concept_uniform_distribution([[1,2],[1,3]], 0.5, 1000, 4)
-    Y = 2*Y - 1
+    
+    # Evaluation
     no_runs = 1
-    sip.evaluate_multiple_times(X, Y, no_runs)
-    print('Optimal w:')
-    w_optim_trimed = [w if w>0.01 else 0.0 for w in sip.w_optimal]
-    print(w_optim_trimed)
-    print(f'Ro:{sip.ro}')
+    test_size = 0.2
+    coefficient_threshold = 0.01
+    EvaluationTools.evaluate_multiple_times(dipnn, X, Y, no_runs, test_size, coefficient_threshold)
+    print(f'The margin: {dipnn.ro}')
+
+def experiment_growth_degree2_no_fixed_margin():
+    # Data 
+    conjunctions = [[1,2],[1,3], [2,4]]
+    p = 0.5
+    dataset_size = 1000
+    no_variables = 20
+    X, Y = boolean_concept_uniform_distribution(conjunctions, p, dataset_size, no_variables)
+
+    # Model
+    d_max = 2
+    balance = 1.0
+    lambda_param = 1.0
+    ro = 1.0
+    fixed_margin = False
+    dipnn = DeepInterpretablePolynomialNeuralNetwork(d_max=d_max, lambda_param=lambda_param, balance=balance, fixed_margin=fixed_margin, ro=ro, derivative_magnitude_th=0.0, coeff_magnitude_th=0.0, 
+                                        max_no_terms_per_iteration=10, max_no_terms=200, growth_policy=GrowthPolicy.SELECT_BY_DERIVATIVE)
+    
+    # Evaluation
+    no_runs = 1
+    test_size = 0.2
+    coefficient_threshold = 0.01
+    EvaluationTools.evaluate_multiple_times(dipnn, X, Y, no_runs, test_size, coefficient_threshold)
+    print(f'The margin: {dipnn.ro}')
 
 if __name__ == '__main__':
-    basic_experiment_no_growth()
+    experiment_growth_degree2_no_fixed_margin()
