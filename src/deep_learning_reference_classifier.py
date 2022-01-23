ee46056ddbf51200b2a1d5242c19f37c81f4860f
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2022-01-23 08:24:42 +0200

    Small correction to ref classifier

--- src/deep_learning_reference_classifier.py
+++ src/deep_learning_reference_classifier.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 
 class DeepLearningReferenceClassifier:
    
-    def train(self, X_train, Y_train,  epochs, no_classes, no_hidden_layers, size_of_hidden_layers):
+    def train_network1(self, X_train, Y_train,  X_test, Y_test, epochs, no_classes, no_hidden_layers, size_of_hidden_layers, class_weights=None):
         no_features = len(X_train[0])
         layers = [tf.keras.layers.Dense(no_features, activation='relu')]
         for i in range(no_hidden_layers):
@@ -18,26 +18,42 @@ class DeepLearningReferenceClassifier:
         if no_classes < 2:
             raise Exception("At least two classes are expected (no_classes>=2)") 
         elif no_classes == 2:
-            loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+            loss = tf.keras.losses.BinaryCrossentropy()
         else:
             loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         self.model.compile(optimizer='adam',
                     loss=loss,
                     metrics=['accuracy'])
         
-        self.model.fit(X_train, Y_train, epochs=epochs)
-        
+        self.model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=epochs, class_weight=class_weights)
+    
+    def train_network2(self, X_train, Y_train,  X_test, Y_test, epochs, no_classes, no_hidden_layers, size_of_hidden_layers, class_weights=None):
+        self.model = tf.keras.Sequential()
+        self.model.add(tf.keras.layers.Dense(10, input_dim=len(X_train[0]), activation='relu', kernel_initializer="uniform"))
+        self.model.add(tf.keras.layers.Dropout(0.2))
+        self.model.add(tf.keras.layers.Dense(5, activation='relu', kernel_constraint=tf.keras.constraints.MaxNorm(max_value=3)
+, kernel_initializer="uniform"))
+        self.model.add(tf.keras.layers.Dropout(0.2))
+        self.model.add(tf.keras.layers.Dense(5, activation='relu', kernel_initializer="uniform"))
+        self.model.add(tf.keras.layers.Dense(1, activation='sigmoid', kernel_initializer="uniform"))
+
+        # Compile model
+        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        # Fit the model
+        self.model.fit(X_train, Y_train, epochs=epochs, batch_size=10, class_weight=class_weights)
+
+        # Evaluate the model
+        scores = self.model.evaluate(X_test, Y_test)
+        print("%s: %.2f%%" % (self.model.metrics_names[1], scores[1]*100))
     
     def predict(self, X_test):
-        probability_model = tf.keras.Sequential([self.model, 
-                                         tf.keras.layers.Softmax()])
-        predictions_probabilities = probability_model.predict(X_test)
-        predictions = np.argmax(predictions_probabilities, axis=1)
-        return predictions, predictions_probabilities
+        predictions = self.model.predict_classes(X_test)
+        return predictions
 
 
     def test(self, X_test, Y_test):
-        Y_predicted_binary,Y_predicted = self.predict(X_test)
+        Y_predicted_binary = self.predict(X_test)
         no_errors = 0.0 
         N = 0.0 
         P = 0.0 
@@ -64,33 +80,31 @@ class DeepLearningReferenceClassifier:
             TN_rate = 1.0
         else:
             TN_rate = TN/N
-        roc_auc_score_value = roc_auc_score([1.0 if y >  0.5 else 0.0 for y in Y_test], Y_predicted[:,0])
+        #roc_auc_score_value = roc_auc_score([1.0 if y >  0.5 else 0.0 for y in Y_test], Y_predicted[:,0])
 
-        return 1.0 - no_errors/float(len(Y_predicted_binary)), TP_rate, TN_rate, roc_auc_score_value
+        return 1.0 - no_errors/float(len(Y_predicted_binary)), TP_rate, TN_rate
 
-    def evaluate_multiple_times(self, X, Y, no_runs, test_size, epochs, no_classes, no_hidden_layers, size_of_hidden_layers):
+    def evaluate_multiple_times(self, X, Y, no_runs, test_size, epochs, no_classes, no_hidden_layers, size_of_hidden_layers, class_weights=None):
         sum_acc = 0.0
         sum_TP_rate = 0.0
         sum_TN_rate = 0.0
-        sum_roc_auc = 0.0
         accuracy_list = []
         training_time = []
         test_time = []
         for k in range(no_runs):
             X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size)
             start_training = time.time()
-            self.train(X_train,Y_train, epochs, no_classes, no_hidden_layers, size_of_hidden_layers)
+            self.train_network2(X_train, Y_train, X_test, Y_test, epochs, no_classes, no_hidden_layers, size_of_hidden_layers, class_weights)
             end_training = time.time()
             training_time.append(end_training-start_training)
             start_test = time.time()
-            acc, TP_rate, TN_rate, roc_auc = self.test(X_test, Y_test)
+            acc, TP_rate, TN_rate = self.test(X_test, Y_test)
             end_test = time.time()
             test_time.append(end_test-start_test) # Include some additional processing 
             accuracy_list.append(acc)
             sum_acc += acc
             sum_TP_rate += TP_rate
             sum_TN_rate += TN_rate
-            sum_roc_auc += roc_auc
         avg_acc = sum_acc/float(no_runs)
         var_acc = (np.sum((np.array(accuracy_list) - avg_acc)**2))/float(no_runs) 
         avg_training_time = sum(training_time)/float(no_runs)
@@ -100,8 +114,7 @@ class DeepLearningReferenceClassifier:
         print(f'Average accuracy: {avg_acc}')
         print(f'Variance of accuracy: {var_acc}')
         print(f'Average true positive rate: {sum_TP_rate/float(no_runs)}')
-        print(f'Averagevg true negative rate: {sum_TN_rate/float(no_runs)}')
-        print(f'Area Under the Receiver Operating Characteristic Score: {sum_roc_auc/float(no_runs)}')
+        print(f'Average true negative rate: {sum_TN_rate/float(no_runs)}')
         print(f'Average training time: {avg_training_time}')
         print(f'Variance of training time: {var_training_time}')
         print(f'Average test time: {avg_test_time}')
