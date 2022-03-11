78103301f1940ecb2a64468f0dc79e69621a7a81
Popescu Claudiu <popescu_claudiu1989@yahoo.com>
2022-03-11 17:47:19 +0200

    Update running time experiments

--- experiments/experiments_running_time.py
+++ experiments/experiments_running_time.py
@@ -1,3 +1,11 @@
+'''
+This module contains experiments regarding the running time. 
+See 7.7 Experiment 7: the running time analysis.
+
+Before running the experiments, be sure you download and copy the CSV file located at https://archive.ics.uci.edu/ml/datasets/adult (Accessed 22.01.2022)
+in the "data" folder.
+'''
+
 import csv
 import sys
 import numpy as np
@@ -91,11 +99,11 @@ def convert_to_logical_values(X):
             new_row.append(float(value - min[i])/(max[i]-min[i]))
         new_X.append(new_row)
     return np.array(new_X)
-
+# EXPERIMENTS REPORTED IN THE PAPER
 def experiment_running_time_vs_number_of_features():
     # Data
     X_original, Y, features_names = read_data('./data/adult.data')
-    
+    print(len(X_original))
     # Model
     d_max = 1
     balance = 2.0
@@ -109,7 +117,7 @@ def experiment_running_time_vs_number_of_features():
     test_size = 0.2
     coefficient_threshold = 0.01
     precision = 2
-    n_features_values = range(5,100, 5)
+    n_features_values = range(5, 100, 5)
     avg_training_time_list = list()
     var_training_time_list = list()
     avg_test_time_list = list()
@@ -118,7 +126,7 @@ def experiment_running_time_vs_number_of_features():
         print(f'Experiment with {n_features} features')
         X = X_original[:,range(n_features)]
         # Evaluation
-        _, avg_training_time, var_training_time, avg_test_time, var_test_time = EvaluationTools.evaluate_multiple_times(dipnn, X, Y, no_runs, test_size, coefficient_threshold, precision)
+        avg_acc, avg_training_time, var_training_time, avg_test_time, var_test_time, avg_tp_rate, avg_tn_rate = EvaluationTools.evaluate_multiple_times(dipnn, X, Y, no_runs, test_size, coefficient_threshold, precision)
         avg_training_time_list.append(avg_training_time)
         var_training_time_list.append(np.sqrt(var_training_time))
         avg_test_time_list.append(avg_test_time)
@@ -130,17 +138,167 @@ def experiment_running_time_vs_number_of_features():
     fig1, ax1 = plt.subplots()
     ax1.plot(n_features_values,avg_training_time_list)
     ax1.fill_between(n_features_values, (avg_training_time_list-var_training_time_list), (avg_training_time_list+var_training_time_list), color='b', alpha=.1)
-    plt.title('Variation of training time with the number of features')
+    plt.title('Evolution of training time with the number of features')
     plt.xlabel('Number of features')
     plt.ylabel('Average time (s)')
 
     fig2, ax2 = plt.subplots()
     ax2.plot(n_features_values,avg_test_time_list)
     ax2.fill_between(n_features_values, (avg_test_time_list-var_test_time_list), (avg_test_time_list+var_test_time_list), color='b', alpha=.1)
-    plt.title('Variation of test time with the number of features')
+    plt.title('Evolution of test time with the number of features')
     plt.xlabel('Number of features')
     plt.ylabel('Average time (s)')
     plt.show()
 
+def experiment_running_time_vs_number_of_data_points():
+    # Data
+    X_original, Y_original, features_names = read_data('./data/adult.data')
+    print(len(X_original[1]))
+    # Model
+    d_max = 1
+    balance = 2.0
+    lambda_param = 10.0
+    ro = 1.0
+    fixed_margin = True
+    growth_policy = GrowthPolicy.ALL_TERMS
+    dipnn = DeepInterpretablePolynomialNeuralNetwork(d_max=d_max, lambda_param=lambda_param, balance=balance, fixed_margin=fixed_margin, ro=ro, derivative_magnitude_th=0.0, coeff_magnitude_th=0.0, 
+                                        max_no_terms_per_iteration=20, max_no_terms=200, growth_policy=growth_policy)
+    no_runs = 10
+    test_size = 0.2
+    coefficient_threshold = 0.01
+    precision = 2
+    n_data_points_values = range(50, 30000, 5000)
+    avg_training_time_list = list()
+    var_training_time_list = list()
+    avg_test_time_list = list()
+    var_test_time_list = list()
+    for n_points in n_data_points_values:
+        print(f'Experiment with {n_points} data points')
+        X = X_original[range(n_points),:]
+        Y = Y_original[range(n_points)]
+        # Evaluation
+        avg_acc, avg_training_time, var_training_time, avg_test_time, var_test_time, avg_tp_rate, avg_tn_rate = EvaluationTools.evaluate_multiple_times(dipnn, X, Y, no_runs, test_size, coefficient_threshold, precision)
+        avg_training_time_list.append(avg_training_time)
+        var_training_time_list.append(np.sqrt(var_training_time))
+        avg_test_time_list.append(avg_test_time)
+        var_test_time_list.append(np.sqrt(var_test_time))
+    avg_training_time_list = np.array(avg_training_time_list)
+    var_training_time_list = np.array(var_training_time_list)
+    avg_test_time_list = np.array(avg_test_time_list)
+    var_test_time_list = np.array(var_test_time_list)
+    fig1, ax1 = plt.subplots()
+    ax1.plot(n_data_points_values,avg_training_time_list)
+    ax1.fill_between(n_data_points_values, (avg_training_time_list-var_training_time_list), (avg_training_time_list+var_training_time_list), color='b', alpha=.1)
+    plt.title('Evolution of training time with the number of data points')
+    plt.xlabel('Number of data points')
+    plt.ylabel('Average time (s)')
+
+    fig2, ax2 = plt.subplots()
+    ax2.plot(n_data_points_values,avg_test_time_list)
+    ax2.fill_between(n_data_points_values, (avg_test_time_list-var_test_time_list), (avg_test_time_list+var_test_time_list), color='b', alpha=.1)
+    plt.title('Evolution of test time with the number of data points')
+    plt.xlabel('Number of data points')
+    plt.ylabel('Average time (s)')
+    plt.show()
+
+def experiment_running_time_vs_max_degree_grow():
+    # Data
+    X_original, Y, features_names = read_data('./data/adult.data')
+    X = X_original[:, :3]
+    print(len(X))
+    # Parameters
+    balance = 1.0
+    lambda_param = 0.01
+    ro = 1.0
+    fixed_margin = True
+    growth_policy = GrowthPolicy.GROW
+    no_runs = 10
+    test_size = 0.2
+    coefficient_threshold = 0.01
+    precision = 2
+    d_max_range = range(1,6,1)
+    avg_training_time_list = list()
+    var_training_time_list = list()
+    avg_test_time_list = list()
+    var_test_time_list = list()
+    for d_max in d_max_range:
+        print(f'Experiment with max degree {d_max}')
+        dipnn = DeepInterpretablePolynomialNeuralNetwork(d_max=d_max, lambda_param=lambda_param, balance=balance, fixed_margin=fixed_margin, ro=ro, derivative_magnitude_th=0.0, coeff_magnitude_th=0.0, 
+                                        max_no_terms_per_iteration=20, max_no_terms=200, growth_policy=growth_policy)
+        # Evaluation
+        avg_acc, avg_training_time, var_training_time, avg_test_time, var_test_time, avg_tp_rate, avg_tn_rate = EvaluationTools.evaluate_multiple_times(dipnn, X, Y, no_runs, test_size, coefficient_threshold, precision)
+        avg_training_time_list.append(avg_training_time)
+        var_training_time_list.append(np.sqrt(var_training_time))
+        avg_test_time_list.append(avg_test_time)
+        var_test_time_list.append(np.sqrt(var_test_time))
+    avg_training_time_list = np.array(avg_training_time_list)
+    var_training_time_list = np.array(var_training_time_list)
+    avg_test_time_list = np.array(avg_test_time_list)
+    var_test_time_list = np.array(var_test_time_list)
+    fig1, ax1 = plt.subplots()
+    ax1.plot(d_max_range,avg_training_time_list, marker='o')
+    ax1.fill_between(d_max_range, (avg_training_time_list-var_training_time_list), (avg_training_time_list+var_training_time_list), color='b', alpha=.1)
+    plt.title('Evolution of training time with the maximum degree')
+    plt.xlabel('d_max')
+    plt.ylabel('Average time (s)')
+
+    fig2, ax2 = plt.subplots()
+    ax2.plot(d_max_range,avg_test_time_list, marker='o')
+    ax2.fill_between(d_max_range, (avg_test_time_list-var_test_time_list), (avg_test_time_list+var_test_time_list), color='b', alpha=.1)
+    plt.title('Evolution of test time with the maximum degree')
+    plt.xlabel('d_max')
+    plt.ylabel('Average time (s)')
+    plt.show()
+
+# OTHER EXPERIMENTS
+def experiment_running_time_vs_max_degree_all_terms():
+    # Data
+    X_original, Y, features_names = read_data('./data/adult.data')
+    X = X_original[:, :3]
+    # Parameters
+    balance = 1.0
+    lambda_param = 10.0
+    ro = 1.0
+    fixed_margin = True
+    growth_policy = GrowthPolicy.ALL_TERMS
+    no_runs = 10
+    test_size = 0.2
+    coefficient_threshold = 0.01
+    precision = 2
+    d_max_range = range(1,6,1)
+    avg_training_time_list = list()
+    var_training_time_list = list()
+    avg_test_time_list = list()
+    var_test_time_list = list()
+    for d_max in d_max_range:
+        print(f'Experiment with max degree {d_max}')
+        dipnn = DeepInterpretablePolynomialNeuralNetwork(d_max=d_max, lambda_param=lambda_param, balance=balance, fixed_margin=fixed_margin, ro=ro, derivative_magnitude_th=0.0, coeff_magnitude_th=0.0, 
+                                        max_no_terms_per_iteration=20, max_no_terms=200, growth_policy=growth_policy)
+        # Evaluation
+        avg_acc, avg_training_time, var_training_time, avg_test_time, var_test_time, avg_tp_rate, avg_tn_rate = EvaluationTools.evaluate_multiple_times(dipnn, X, Y, no_runs, test_size, coefficient_threshold, precision)
+        avg_training_time_list.append(avg_training_time)
+        var_training_time_list.append(np.sqrt(var_training_time))
+        avg_test_time_list.append(avg_test_time)
+        var_test_time_list.append(np.sqrt(var_test_time))
+    avg_training_time_list = np.array(avg_training_time_list)
+    var_training_time_list = np.array(var_training_time_list)
+    avg_test_time_list = np.array(avg_test_time_list)
+    var_test_time_list = np.array(var_test_time_list)
+    fig1, ax1 = plt.subplots()
+    ax1.plot(d_max_range,avg_training_time_list, marker='o')
+    ax1.fill_between(d_max_range, (avg_training_time_list-var_training_time_list), (avg_training_time_list+var_training_time_list), color='b', alpha=.1)
+    plt.title('Evolution of training time with the maximum degree')
+    plt.xlabel('d_max')
+    plt.ylabel('Average time (s)')
+
+    fig2, ax2 = plt.subplots()
+    ax2.plot(d_max_range,avg_test_time_list, marker='o')
+    ax2.fill_between(d_max_range, (avg_test_time_list-var_test_time_list), (avg_test_time_list+var_test_time_list), color='b', alpha=.1)
+    plt.title('Evolution of test time with the maximum degree')
+    plt.xlabel('d_max')
+    plt.ylabel('Average time (s)')
+    plt.show()
 if __name__ == '__main__':
+    experiment_running_time_vs_number_of_data_points()
+    experiment_running_time_vs_max_degree_grow()
     experiment_running_time_vs_number_of_features()
